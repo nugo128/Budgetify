42780a1997152be9b90723014715f27ed149e3fc
nugo128 <nugo.rostiashvili@redberry.ge>
2024-02-05 10:18:50 +0400

    markup for registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordInputComponent } from './password-input/password-input.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PasswordInputComponent
+    PasswordInputComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
