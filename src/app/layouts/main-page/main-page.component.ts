cb3b10b2d7305e2c3dd5f3cc189d66626d1f9c65
nugo128 <nugo.rostiashvili@redberry.ge>
2024-02-15 18:35:13 +0400

    UI-16: Create MatDialog for transaction information

--- src/app/layouts/main-page/main-page.component.ts
+++ src/app/layouts/main-page/main-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionService } from '../../services/transaction.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../dialog/dialog.component';
+import { ITransaction } from '../../models/transaction';
 
 @Component({
   selector: 'app-main-page',
@@ -7,13 +10,31 @@ import { TransactionService } from '../../services/transaction.service';
   styleUrl: './main-page.component.css',
 })
 export class MainPageComponent implements OnInit {
-  public transactions;
-  constructor(private transactionService: TransactionService) {}
+  public transactions: ITransaction;
+  constructor(
+    private transactionService: TransactionService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    this.transactionService.getUser().subscribe((responseData) => {
+    this.transactionService.getTransactions().subscribe((responseData) => {
       console.log(responseData['transactions']);
       this.transactions = responseData['transactions'];
+    });
+  }
+  openDialog(data: any) {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: data,
+      panelClass: 'custom-dialog-container',
+      position: {
+        top: '0px',
+        right: '0px',
+      },
+      height: '100%',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
     });
   }
 }
---
+++ src/app/models/transaction.ts
@@ -0,0 +1,9 @@
+export interface ITransaction {
+  id: number;
+  amount: number;
+  author: string;
+  date: string;
+  description: string;
+  title: string;
+  transaction_type: string;
+}
