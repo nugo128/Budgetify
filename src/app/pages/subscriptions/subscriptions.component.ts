f82c53df102ce5a1ac50b8076084e7b5815ab323
nugo128 <nugo.rostiashvili@redberry.ge>
2024-03-03 13:45:43 +0400

    subscriptions page

--- src/app/pages/subscriptions/subscriptions.component.ts
+++ src/app/pages/subscriptions/subscriptions.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { IAccount } from '../../models/account';
+import { AccountDialogComponent } from '../../components/account-dialog/account-dialog.component';
+import { AccountService } from '../../services/account.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-subscriptions',
   templateUrl: './subscriptions.component.html',
-  styleUrl: './subscriptions.component.css'
+  styleUrl: './subscriptions.component.css',
 })
 export class SubscriptionsComponent {
+  accounts: IAccount[];
+  constructor(
+    public accountService: AccountService,
+    public dialog: MatDialog
+  ) {}
 
+  ngOnInit(): void {
+    this.accountService.getAccounts().subscribe((response) => {
+      this.accounts = response['accounts'];
+    });
+  }
+  openAccountDialog(data: IAccount) {
+    const dialogRef = this.dialog.open(AccountDialogComponent, {
+      data: data,
+      panelClass: 'custom-dialog-container',
+      position: {
+        top: '0px',
+        right: '0px',
+      },
+      height: '100%',
+      width: '603px',
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      const index = this.accounts.findIndex(
+        (obj: IAccount) => obj.id === result.id
+      );
+      this.accounts[index] = result;
+    });
+  }
 }
