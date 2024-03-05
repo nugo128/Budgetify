df1ab7ef8e3816fa1be2b48948ea5ad11bbc4a47
nugo128 <nugo.rostiashvili@redberry.ge>
2024-03-05 15:35:28 +0400

    obligatory page

--- src/app/pages/obligatory/obligatory.component.ts
+++ src/app/pages/obligatory/obligatory.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ISubscription } from '../../models/subscription';
+import { AccountService } from '../../services/account.service';
+import { AccountDialogComponent } from '../../components/account-dialog/account-dialog.component';
+import { IAccount } from '../../models/account';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-obligatory',
   templateUrl: './obligatory.component.html',
-  styleUrl: './obligatory.component.css'
+  styleUrl: './obligatory.component.css',
 })
-export class ObligatoryComponent {
+export class ObligatoryComponent implements OnInit {
+  subscriptions: ISubscription[];
+  accounts: any;
+  constructor(
+    public accountService: AccountService,
+    public dialog: MatDialog
+  ) {}
+  ngOnInit(): void {
+    this.accountService.getAccounts().subscribe((response) => {
+      this.accounts = response['accounts'];
+      this.accounts[0].active = true;
+    });
+  }
 
+  openAccountDialog(data: IAccount) {
+    const dialogRef = this.dialog.open(AccountDialogComponent, {
+      data: data,
+      panelClass: 'custom-dialog-container',
+      position: {
+        top: '0px',
+        right: '0px',
+      },
+      height: '100%',
+      width: '603px',
+    });
+    const index = this.accounts.findIndex(
+      (obj: IAccount) => obj.id === data.id
+    );
+    this.accounts.forEach((item) => {
+      item.active = false;
+    });
+    this.accounts[index].active = true;
+    dialogRef.afterClosed().subscribe((result) => {
+      const index = this.accounts.findIndex(
+        (obj: IAccount) => obj.id === result.id
+      );
+      this.accounts[index] = result;
+    });
+  }
 }
