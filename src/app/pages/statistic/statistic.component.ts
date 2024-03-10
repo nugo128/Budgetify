fa330f0761135aaf137d524007671c92e177c2ff
nugo128 <nugo.rostiashvili@redberry.ge>
2024-03-10 20:09:40 +0400

    Bar chart for categories statistic

--- src/app/pages/statistic/statistic.component.ts
+++ src/app/pages/statistic/statistic.component.ts
@@ -5,6 +5,7 @@ import { IAccount } from '../../models/account';
 import { MatDialog } from '@angular/material/dialog';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { StatisticService } from '../../services/statistic.service';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-statistic',
@@ -17,7 +18,59 @@ export class StatisticComponent {
   groupedTransactions: any;
   amount: any = 0;
   displayedColumns: string[] = ['Category', 'Amount', '% in total'];
-  dataSoutce: any;
+  dataSource: any;
+  chartDom;
+  myChart;
+  option = {
+    grid: {
+      left: '0%',
+      right: '65%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'category',
+      data: [],
+      name: 'Category',
+      nameLocation: 'end',
+      nameGap: 10,
+      axisLabel: { show: false },
+      axisLine: {
+        symbol: ['none', 'arrow'],
+        symbolSize: [8, 8],
+        symbolOffset: [5, 5],
+        show: true,
+        lineStyle: {
+          color: 'black',
+        },
+      },
+    },
+    yAxis: {
+      type: 'value',
+      name: '$   ',
+      nameGap: 10,
+      splitLine: {
+        show: false,
+      },
+      axisLine: {
+        symbol: ['none', 'arrow'],
+        symbolSize: [8, 8],
+        symbolOffset: [5, 5],
+        show: true,
+        lineStyle: {
+          color: 'black',
+        },
+      },
+      axisTick: {
+        show: true,
+        alignWithLabel: true,
+      },
+      nameTextStyle: {
+        align: 'right',
+      },
+    },
+    series: [],
+  };
   constructor(
     private accountService: AccountService,
     public dialog: MatDialog,
@@ -25,8 +78,8 @@ export class StatisticComponent {
     public statisticService: StatisticService
   ) {
     this.statistic = this.fb.group({
-      date_from: '',
-      date_to: '',
+      date_from: '1/3/2023',
+      date_to: '3/3/2024',
     });
   }
   accounts: any;
@@ -34,6 +87,64 @@ export class StatisticComponent {
     this.accountService.getAccounts().subscribe((response) => {
       this.accounts = response['accounts'];
       this.accounts[0].active = true;
+    });
+    this.chartDom = document.getElementById('chart');
+    this.myChart = echarts.init(this.chartDom);
+
+    this.statisticService.get(this.statistic.value).subscribe((response) => {
+      this.statisticData = response;
+      const groupedMap = new Map<string, any>();
+
+      this.statisticData.forEach((transaction) => {
+        const hasTransactionType = 'transaction_type' in transaction;
+
+        if (
+          (hasTransactionType && transaction.transaction_type === 'Expenses') ||
+          !hasTransactionType
+        ) {
+          const categoryObject = JSON.parse(transaction.category);
+          const categoryKeys = Object.keys(categoryObject);
+          const category =
+            categoryKeys.length > 0
+              ? categoryObject[categoryKeys[0]]
+              : 'Uncategorized';
+
+          if (!groupedMap.has(category)) {
+            groupedMap.set(category, {
+              category,
+              totalAmount: transaction.amount,
+              transactions: [transaction],
+            });
+          } else {
+            const group = groupedMap.get(category);
+            group.totalAmount += transaction.amount;
+            group.transactions.push(transaction);
+          }
+        }
+      });
+      this.groupedTransactions = Array.from(groupedMap.values());
+      this.groupedTransactions.forEach((element) => {
+        this.amount += element.totalAmount;
+        this.option.series.push({
+          data: [{ value: element.totalAmount, name: element.category }],
+          type: 'bar',
+          barWidth: 20,
+          itemStyle: {
+            color: this.getRandomColor(),
+          },
+          label: {
+            show: true,
+            rotate: '90',
+            formatter: function (params) {
+              return params.data.name;
+            },
+            position: 'top',
+            distance: '20',
+          },
+        });
+      });
+      this.dataSource = this.groupedTransactions;
+      this.myChart.setOption(this.option);
     });
   }
   openAccountDialog(data: IAccount) {
@@ -61,15 +172,26 @@ export class StatisticComponent {
       this.accounts[index] = result;
     });
   }
+  getRandomColor(): string {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+    return color;
+  }
   formData: any;
   dateChanged(start: string, end: string): void {
     this.formData = this.statistic.value;
     this.formData.date_from = start;
     this.formData.date_to = end;
     if (this.formData.date_from && this.formData.date_to) {
-      console.log(this.formData);
       this.statisticService.get(this.formData).subscribe((response) => {
-        console.log(response);
+        this.option.series = [];
+        this.option.xAxis.data = [];
+        this.myChart.clear();
+        this.amount = 0;
         this.statisticData = response;
         const groupedMap = new Map<string, any>();
 
@@ -102,11 +224,28 @@ export class StatisticComponent {
           }
         });
         this.groupedTransactions = Array.from(groupedMap.values());
-        console.log(this.groupedTransactions);
         this.groupedTransactions.forEach((element) => {
           this.amount += element.totalAmount;
+          this.option.series.push({
+            data: [{ value: element.totalAmount, name: element.category }],
+            type: 'bar',
+            barWidth: 20,
+            itemStyle: {
+              color: this.getRandomColor(),
+            },
+            label: {
+              show: true,
+              rotate: '90',
+              formatter: function (params) {
+                return params.data.name;
+              },
+              position: 'top',
+              distance: '20',
+            },
+          });
         });
-        this.dataSoutce = this.groupedTransactions;
+        this.dataSource = this.groupedTransactions;
+        this.myChart.setOption(this.option);
       });
     }
   }
